97d6f56e8bf06200709464a64115c067772bfe1c
enxyo <daniel.gruve@gmail.com>
2018-02-21 19:05:51 +0100

    login signup index init

---
+++ public/signup.php
@@ -0,0 +1,14 @@
+<?php
+    echo "signup";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>FSR Auth</title>
+    </head>
+    <body>
+        The content of the document......
+    </body>
+</html>
