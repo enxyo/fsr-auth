2d97449adc88e2b662ae304cff8ea84fa4661610
enxyo <daniel.gruve@gmail.com>
2018-03-23 17:55:17 +0100

    working logout from dashboard with cookie destruction

--- public/dashboard.php
+++ public/dashboard.php
@@ -21,9 +21,24 @@ if (isset($_COOKIE['fsrAuthCookie'])) {
     // check auth token in db
     if ($db->rowCount() == 1) {
         $userid = $checkToken['userid'];
+
+        // prepare statement
+        $db->query("SELECT * FROM users WHERE users.id = :id");
+        // bind values
+        $db->bind(':id', $userid);
+        // execute
+        $getUser = $db->single();
+
     } else {
-        echo "fail";
+        unset($_COOKIE['fsrAuthCookie']);
+        setcookie('fsrAuthCookie', '', time() - 3600, '/auth/'); // empty value and old timestamp
+
+        header("Location: https://www.free-space-ranger.org:444/auth/signin"); /* Redirect browser */
+        exit();
     }
+} else {
+    header("Location: https://www.free-space-ranger.org:444/auth/signin"); /* Redirect browser */
+    exit();
 }
 ?>
 <!DOCTYPE html>
@@ -60,18 +75,77 @@ if (isset($_COOKIE['fsrAuthCookie'])) {
                     <div class="col-">
                     </div>
                     <div class="col-md text-right">
-                        <span class="text-muted m-3">email@email.com</span>
+                        <span class="text-muted m-3"><?php echo $getUser['email']; ?></span>
                         <a href="#" class="btn btn-outline-danger">
                             <span class="fa fa-cog"></span>
                         </a>
-                        <button type="button" class="btn btn-outline-danger">Logout</button>
+                        <a href="logout" role="button" class="btn btn-outline-danger">Logout</a>
                     </div>
                 </div>
             </div>
         </header>
         <!-- Begin page content -->
         <main role="main" class="container">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Willkommen im FSR Auth System</h5>
+                    <p class="card-text">Wenn du bereits vor dem 1. Mai 2018 bei uns im Forum einen Account erstellt hast, verknüpfe diesen bitte.
+                        <br><br>Seit dem 1. Mai 2018 ist eine Registrierung nur noch über das FSR Auth System möglich.
+                        <br>Dein Forum Account wurde bei der Registrierung automatisch angelegt und du kannst dich mit der hier hinterlegten Email und Passwort im Forum anmelden.
+                        <br><br>Bei Problem bitte bei Nathan Yates melden. <a href="mailto:nathan.yates@free-space-ranger.org">nathan.yates@free-space-ranger.org</a>
+                    </p>
+                    <a target="_blank" href="https://www.free-space-ranger.org/forum/index.php?app=core&module=global&section=login" class="btn btn-primary">Forum</a>
+                </div>
+            </div>
+            <div class="card-deck mt-3">
+                <div class="card" id="legacy_true" style="width: 18rem; border-color: red; display: none;">
+                    <img class="card-img-top" src="img/forum.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title"><span class="fa fa-link"></span> Forum Account</h5>
+                        <span class="text-danger">Account nicht verknüpft!</span>
+                        <p class="card-text">Bitte verknüpfe deinen Foren Account mit deinem Auth Account.</p>
+                        <a href="#" class="btn btn-primary"><span class="fa fa-link"></span> Account verknüpfen</a>
+                    </div>
+                </div>
 
+                <div class="card" id="legacy_false" style="width: 18rem; border-color: green; display: none;">
+                    <img class="card-img-top" src="img/forum.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title"><span class="fa fa-link"></span> Forum Account</h5>
+                        <span class="text-success">Account verknüpft!</span>
+                        <p class="card-text">Deinen Foren Account ist mit deinem Auth Account verknüpft.</p>
+                    </div>
+                </div>
+                <script>
+                    var legacy = "<?php echo $getUser['legacy']; ?>";
+                    if(legacy == 1){
+                        $("#legacy_true").show();
+                    }
+                    if(legacy == 0){
+                        $("#legacy_false").show();
+                    }
+                </script>
+
+
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="img/eve.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">EVE API</h5>
+                        <p class="card-text">Notwendig für alle EVE Spieler.</p>
+                        <p class="card-text">Verwaltung der Api Schlüssel. Bitte alle FSR Charakter hinterlegen.</p>
+                        <a href="#" class="btn btn-primary"><span class="fa fa-link"></span> API Management</a>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="img/discord.svg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Discord</h5>
+                        <p class="card-text">soon</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
         </main>
         <footer class="footer">
             <div class="container">
